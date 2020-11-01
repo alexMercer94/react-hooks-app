4e7b8e56c508aaed047e299888b6aceb0d0684a8
Alejandro Cruz Valera <alex.c.valera94@hotmail.com>
2020-11-01 15:26:29 -0600

    React memo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from './components/05-useLayoutEffect/LayoutEffect';
+import Memorize from './components/06-memos/Memorize';
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+ReactDOM.render(<Memorize />, document.getElementById('root'));
